9b22470a3e5e2dfaad9b490392bd594f7af520ea
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-03 11:29:37 -0300

    Recebe uma função como parâmetro de outra função

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -3,14 +3,9 @@ from impostos import calcula_ISS, calcula_ICMS
 
 class CalculadorDeImpostos(object):
     def realiza_calculo(self, orcamento, imposto):
-
-        if imposto == 'ISS':
-            imposto_calculado = calcula_ISS(orcamento)
-        elif imposto == 'ICMS':
-            imposto_calculado = calcula_ICMS(orcamento)
-
-        print(imposto_calculado)
-
+        
+        imposto_calculado = imposto(orcamento)
+        print (imposto_calculado)
 
 
 if __name__ == '__main__':
@@ -25,5 +20,5 @@ if __name__ == '__main__':
     calculador = CalculadorDeImpostos()
     orcamento = Orcamento(500)
 
-    calculador.realiza_calculo(orcamento, 'ISS')
-    calculador.realiza_calculo(orcamento, 'ICMS')
+    calculador.realiza_calculo(orcamento, calcula_ISS)
+    calculador.realiza_calculo(orcamento, calcula_ICMS)
