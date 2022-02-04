cef0fb13541176e1f2a36571c207db69e5d96009
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-04 10:01:47 -0300

    Delega aos estados, a responsabilidade do cálculo do desconto do orçamento

--- orcamento.py
+++ orcamento.py
@@ -1,26 +1,48 @@
 # -*- coding: UTF-8 -*-
 # orcamento.py
+
+from abc import ABCMeta, abstractmethod
+
+
+class EstadoDeUmOrcamento(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def aplica_desconto_extra(self, orcamento):
+        pass
+
+
+class EmArovacao(EstadoDeUmOrcamento):
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.02)
+
+
+class Aprovado(EstadoDeUmOrcamento):
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.05)
+
+
+class Reprovado(EstadoDeUmOrcamento):
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception('Orcamentos reprovados não receberam desconto extra')
+
+
+class Finalizado(EstadoDeUmOrcamento):
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception('Orcamentos finalizados não receberam desconto extra')
+
+
 class Orcamento(object):
-
-    EM_APROVACAO = 1
-    APROVADO = 2
-    REPROVADO = 3
-    FINALIZADO = 4
-
     def __init__(self):
         self.__itens = []
-        self.estado_atual = Orcamento.EM_APROVACAO
+        self.estado_atual = EmArovacao()
         self.__desconto_extra = 0
 
     def aplica_desconto_extra(self):
-        if self.estado_atual == Orcamento.EM_APROVACAO:
-            self.__desconto_extra += self.valor * 0.02
-        elif self.estado_atual == Orcamento.APROVADO:
-            self.__desconto_extra += self.valor * 0.05
-        elif self.estado_atual == Orcamento.REPROVADO:
-            raise Exception('Orcamentos reprovados não receberam desconto extra')
-        elif self.estado_atual == Orcamento.FINALIZADO:
-            raise Exception('Orcamentos finalizados não receberam desconto extra')
+        self.estado_atual.aplica_desconto_extra(self)
+
+    def adiciona_desconto_extra(self, desconto):
+        self.__desconto_extra += desconto
 
     # quando a propriedade for acessada, ela soma cada item retornando o valor do orçamento
     @property
@@ -28,7 +50,7 @@ class Orcamento(object):
         total = 0.0
         for item in self.__itens:
             total += item.valor
-        return total
+        return total - self.__desconto_extra
 
     # retornamos uma tupla, já que não faz sentido alterar os itens de um orçamento
     def obter_itens(self):
@@ -41,6 +63,7 @@ class Orcamento(object):
 
     def adiciona_item(self, item):
         self.__itens.append(item)
+
 
 # um item criado não pode ser alterado, suas propriedades são somente leitura
 class Item(object):
@@ -56,3 +79,16 @@ class Item(object):
     @property
     def nome(self):
         return self.__nome
+
+
+if __name__ == '__main__':
+    from orcamento import Orcamento, Item
+
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('ITEM-1', 100))
+    orcamento.adiciona_item(Item('ITEM-2', 50))
+    orcamento.adiciona_item(Item('ITEM-3', 400))
+
+    print(orcamento.valor)
+    orcamento.aplica_desconto_extra()
+    print(orcamento.valor)
